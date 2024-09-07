a907e6bf4d4e574215a5dd29c3af8b5f7568219b
edgarlarasanchez <edgar.lara7681@gmail.com>
2024-09-07 18:36:47 -0500

    Added the 'operate' function.
    Simply takes in two number and an operator and calls the function needed.

--- calc.js
+++ calc.js
@@ -14,3 +14,22 @@ function divide(n1,n2){
     return n1/n2;
 }
 
+let firstNumber;
+let secondNumber;
+let operator;
+
+function operate(num1,num2,op){
+    switch(op){
+        case '+':
+            return add(num1,num2);
+        case '-':
+            return subtract(num1,num2);
+        case '*':
+            return multiply(num1,num2);
+        case '/':
+            return divide(num1,num2);
+        default:
+            return "Invalid";
+
+    }
+}
